304fb500e43dfcab32505a3d015b29a04d41dc72
tianhang <476855396@qq.com>
2018-01-27 21:01:50 +0800

    first commit

---
+++ src/main/java/com/thmall/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.thmall.dao;
+
+import com.thmall.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
